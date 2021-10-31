6c4b66e63057195a5d7e7d44609386648cf9d864
EasySocHacks <easy.soc.hacks@gmail.com>
2021-10-31 13:38:51 +0300

    Add argument-based program start

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,55 @@
 import search.IntellijIdeaIshSearcher
+import java.io.File
+import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
-    val intellijIdeaIshSearcher = IntellijIdeaIshSearcher(
-        listOf(
-            "a.b.FooBarBaz",
-            "c.d.FooBar",
-            "liptsoft.WishMaker",
-            "liptsoft.MindReader",
-            "TelephoneOperator",
-            "ScubaArgentineOperator",
-            "YoureLeavingUsHere",
-            "YouveComeToThisPoint",
-            "YourEyesAreSpinningInTheirSockets"
-        )
-    )
+    val usage = "Usage: Main.kt -f <filename> -p <pattern>";
 
-    println(intellijIdeaIshSearcher.search("*"))
+    var filename: String? = null
+    var pattern: String? = null
+
+    var command: String? = null
+    for (i in args.indices) {
+        val arg = args[i]
+
+        when (command) {
+            "filename" -> filename = arg
+            "pattern" -> pattern = arg
+            else ->
+                if (i != args.size - 1) {
+                    command = when (arg) {
+                        "-f" -> "filename"
+                        "--filename" -> "filename"
+                        "-p" -> "pattern"
+                        "--pattern" -> "pattern"
+                        else -> {
+                            println(usage)
+                            exitProcess(0)
+                        }
+                    }
+
+                    continue
+                } else {
+                    println(usage)
+                    exitProcess(0)
+                }
+        }
+
+        command = null
+    }
+
+    if (filename == null || pattern == null) {
+        println(usage)
+        exitProcess(0)
+    }
+
+    val classes: MutableList<String> = mutableListOf()
+
+    File(filename).forEachLine {
+        classes.add(it)
+    }
+
+    val intellijIdeaIshSearcher = IntellijIdeaIshSearcher(classes)
+
+    println(intellijIdeaIshSearcher.search(pattern))
 }
